242c51e3a5bc047ec9bcbd20923828dfbfd18ffd
Alex <alexrybalka65@gmail.com>
2023-05-08 10:36:03 +0300

    компонент Section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+// import { Statistics } from './Statistics/Statistics';
+// import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+// import { Notification } from './Notification/Notification';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    return (
+      <>
+        <Section title={'Phonebook'}>
+          {/* <FeedbackOptions
+            onCountGood={this.hendleCountGood}
+            onCountNeutral={this.hendleCountNeutral}
+            onCountBad={this.hendleCountBad}
+          /> */}
+        </Section>
+        <Section title={'Contacts'}>
+          {/* {(this.state.good || this.state.neutral || this.state.bad) !== 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message={'There is no feedback'} />
+          )} */}
+        </Section>
+      </>
+    );
+  }
+}
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
