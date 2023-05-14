d920adc84fd7d4e923786c1aa0255cb9ab5956fa
Alex <alexrybalka65@gmail.com>
2023-05-14 11:18:37 +0300

    next time

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { SectionTitle, Title } from './SectionStyled';
 
 export const Section = ({ title, children }) => (
